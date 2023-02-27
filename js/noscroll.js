a9f6e48b4c57b5d9db3df9376d691f231b03ca4d
vdtvt <stanislawgzielinski@gmail.com>
2023-02-27 14:32:45 +0100

    random images and cursor

--- js/noscroll.js
+++ js/noscroll.js
@@ -1,16 +1,19 @@
-window.addEventListener('keydown', function(e) {
-    if(e.keyCode == 32 && e.target == document.body) {
-      e.preventDefault();
-    }
-  });
-
-
 
   images_array = [
-    "housenumbers-1.jpg",
-    "housenumbers-2.jpg",
-    "housenumbers-3.jpg",
-    "housenumbers-4.jpg",
-    "housenumbers-5.jpg",
-    "housenumbers-6.jpg",
+    "housenumbers-1.JPG",
+    "housenumbers-2.JPG",
+    "housenumbers-3.JPG",
+    "housenumbers-4.JPG",
+    "housenumbers-5.JPG",
+    "housenumbers-6.JPG",
+    "housenumbers-7.JPG"
+
   ]
+
+
+
+function get_random_image(){
+  random_index = Math.floor(Math.random() * images_array.length);
+  selected_image = images_array[random_index];
+  document.getElementById('main-img').src = `assets/images/${selected_image}`;
+}
